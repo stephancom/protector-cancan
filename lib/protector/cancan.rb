12ba4f846600b4b341598fa2328c4b6b5698139a
stephan.com <stephan@stephan.com>
2018-07-25 15:32:41 -0400

    cancancan dropped inherited_resource support

--- lib/protector/cancan.rb
+++ lib/protector/cancan.rb
@@ -7,4 +7,3 @@ require 'protector/cancan/version'
 
 CanCan::Ability.send :include, Protector::CanCan::Ability
 CanCan::ControllerResource.send :include, Protector::CanCan::Resource
-CanCan::InheritedResource.send :include, Protector::CanCan::Resource
