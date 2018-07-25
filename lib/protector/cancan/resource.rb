dfc303586772e2961d7fc2a2fdb7fe6ab1728bb4
stephan.com <stephan@stephan.com>
2018-07-25 15:18:54 -0400

    Remove deprecated alias_method_chain
    Doing it the crude way without using `prepend` for simplicity.

--- lib/protector/cancan/resource.rb
+++ lib/protector/cancan/resource.rb
@@ -2,9 +2,12 @@ module Protector
   module CanCan
     module Resource extend ActiveSupport::Concern
       included do
-        alias_method_chain :resource_base, :protector
-        alias_method_chain :load_collection, :protector
-        alias_method_chain :load_collection?, :protector
+        alias_method :resource_base_without_protector, :resource_base
+        alias_method :resource_base, :resource_base_with_protector
+        alias_method :load_collection_without_protector, :load_collection
+        alias_method :load_collection, :load_collection_with_protector
+        alias_method :load_collection_without_protector, :load_collection
+        alias_method :load_collection, :load_collection_with_protector
       end
 
       def resource_base_with_protector
